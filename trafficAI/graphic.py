65859435e32ca9f3a1be8c6315b49a1397fb3e27
Vojtech <kudlacek.vojtech@sspbrno.cz>
2018-11-23 11:36:54 +0100

    _get_source_info done

--- trafficAI/graphic.py
+++ trafficAI/graphic.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 class Presenter:
     def __init__(self):
         pass
@@ -20,14 +23,22 @@ class Presenter:
     def _draw_object(self):
         pass
 
-    def _draw_cell(self)
-        pass
-    
-    def _process_connection(self):
+    def _draw_cell(self):
         pass
 
-    def _get_source_info(self):
-        pass
+    def _process_connection(self, source, destination):
+
+        src, dest, vect = self._get_source_info(source, dest)
+        
+
+    def _get_source_info(self, code1, code2):
+        source = self.point_location[code1]
+        dest = self.point_location[code2]
+
+        vector = (source[0] - dest[0], source[1] - dest[1])
+        if vector not in [(0,1),(1,0),(-1,0),(0,-1)]:
+            raise RuntimeError("Unexpected direction")
+        return source[0], source[1], vector
 
     def _calculate_start(self):
         pass
