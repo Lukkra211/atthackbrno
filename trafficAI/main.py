ee14fbb72b669758900a9cbad0a15f61d68c20ac
Lukas Krajicek <lkrajice@redhat.com>
2018-11-23 16:12:37 +0100

    Add mode to CLI

--- trafficAI/main.py
+++ trafficAI/main.py
@@ -6,9 +6,15 @@ from control import Controller
 
 @click.command()
 @click.option('--system', type=click.Path(exists=True))
-def main(system):
+@click.option('--mode', type=click.Choice(['run', 'train']))
+def main(system, mode):
     controller = Controller()
     controller.load_system(system)
+
+    if mode == 'run':
+        controller.present()
+    elif mode == 'train':
+        controller.develop()
 
 
 if __name__ == '__main__':
