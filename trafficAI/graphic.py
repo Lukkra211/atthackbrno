a1f8f7154ff7b7b3371115be91014d05b2602dbc
JakubMicanek <jakubak1@seznam.cz>
2018-11-23 14:30:48 +0100

    2d_map

--- trafficAI/graphic.py
+++ trafficAI/graphic.py
@@ -2,11 +2,40 @@ import pygame
 import sys
 import os
 
-cell_colors = {}
-length = 3
-size = (length, length)
-"screensize"
+arr =   [[0,0,1,1,1,1,1,1,1,0,0,],
+         [0,1,1,0,0,0,0,0,1,1,0,],
+         [1,1,0,0,0,0,0,0,0,1,1,],
+         [1,0,0,0,0,0,0,0,0,0,1,],
+         [1,0,0,0,1,1,1,0,0,0,1,],
+         [1,0,0,0,1,1,1,0,0,0,1,],
+         [1,0,0,0,1,1,1,0,0,0,1,],
+         [1,0,0,0,0,0,0,0,0,0,1,],
+         [1,1,0,0,0,0,0,0,0,1,1,],
+         [0,1,1,0,0,0,0,0,1,1,0,],
+         [0,0,1,1,1,1,1,1,1,0,0,]]
 
+minimap =  [['a01', 'j01', 'a02'],
+            ['a03', 'j05', 'a07'],
+            ['a08', 'j10', 'a12']]
+
+check = 1
+for i in range(len(minimap)):
+    for j in range(len(minimap[i])):
+        if len(minimap[i]) > check:
+            check = len(minimap[i])
+
+cell_empty = 0
+cell_filled = 1
+black = (0,0,0)
+white = (255,255,255)
+cell_colors = {cell_filled: black,cell_empty: white}
+
+length = 4
+size = (length, length)
+screensize = (((len(minimap)*164)-120), ((check*164)-120))
+print(screensize[0],screensize[1])
+
+"""import minimap from somewhere"""
 
 class Presenter:
     def __init__(self):
@@ -18,10 +47,9 @@ class Presenter:
         pygame.display.set_caption("something")
 
         screen = pygame.display.set_mode(screensize)
-        screen.fill(255, 255, 255)
-        pygame.display.flip()
+        screen.fill((255, 255, 255))
 
-    def _process(self):
+    def process(self):
         pass
 
     def main_loop(self):
@@ -35,8 +63,8 @@ class Presenter:
 
     @staticmethod
     def _draw_object(horizontal, vertical, array):
-        for row, hor_s in enumerate(array):
-            for ver_s, cell in enumerate(Iterable(row)):
+        for hor_s, row in enumerate(array):
+            for ver_s, cell in enumerate(row):
                 hor = horizontal + hor_s
                 ver = vertical + ver_s
                 Presenter._draw_cell(hor, ver, cell_colors[cell])
@@ -46,8 +74,9 @@ class Presenter:
         px = x * length
         py = y * length
 
-        rectangle = pygame.rect((px, py), size)
+        rectangle = pygame.Rect((px, py), size)
         pygame.draw.rect(pygame.display.get_surface(), color, rectangle)
+        pygame.display.update()
 
     def _process_connection(self):
         pass
@@ -59,5 +88,7 @@ class Presenter:
         pass
 
 
-
+Presenter._init_window(screensize)
+Presenter._draw_cell(0, 0, black)
+input()
 
