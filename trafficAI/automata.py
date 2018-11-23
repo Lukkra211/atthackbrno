f791db9462c4f7c0cacede49a377310c4dab856c
--global <janbuchmaier@seznam.cz>
2018-11-23 11:59:10 +0100

    Added Core __main__ and predefined methods

--- trafficAI/automata.py
+++ trafficAI/automata.py
@@ -1,3 +1,7 @@
+from numpy import random
+from itertools import chain
+
+
 class Link:
     pass
 
@@ -19,4 +23,52 @@ class JunctionPoint:
 
 
 class Core:
-    pass
+
+    TYPES = {a: AccessPoint,
+             j: JunctionPoint,
+             l: LinkPoint,
+        }
+
+    def __init__(self, minimap: list, connections: list, vehicles: int):
+
+        self.vehicles = vehicles
+        self.access_points = []
+        self.junction_points = []
+        self.junction_queues = []
+        self.points = {}
+
+        self.nn = False
+        self.__process(minimap, connections)
+
+    def __process(self, minimap, connections):
+        for code in chain.from_iterable(minimap):
+            if not code:
+                continue
+
+            point = Point(code = code)
+            self.points.appent(point)
+
+            if code[0] == "a":
+                point.callback = self.spawn
+                self.access_points.append(point)
+
+            if code[0] == "j":
+                self.junction_points.append(point)
+
+    def __exec_nn(self):
+        pass
+
+    def reset(self, nn):
+        pass
+
+    def spawn_vehicle(self):
+        pass
+
+    def create_links(self, code1, code2):
+        pass
+
+    def finalize(self):
+        pass
+
+    def step(self):
+        pass
