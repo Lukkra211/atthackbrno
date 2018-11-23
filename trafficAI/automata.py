3a671ebafb899241c006ebd01f804658bfb3bc07
Lukas Krajicek <lkrajice@redhat.com>
2018-11-23 11:07:37 +0100

    Initial commit with classes for each team

---
+++ trafficAI/automata.py
@@ -0,0 +1,22 @@
+class Link:
+    pass
+
+
+class LinkPoint:
+    pass
+
+
+class Point:
+    pass
+
+
+class AccessPoint:
+    pass
+
+
+class JunctionPoint:
+    pass
+
+
+class Core:
+    pass
---
+++ trafficAI/graphic.py
@@ -0,0 +1,33 @@
+class Presenter:
+    def __init__(self):
+        pass
+
+    def _init_window(self):
+        pass
+
+    def _process(self):
+        pass
+
+    def main_loop(self):
+        pass
+
+    def _redraw_links(self):
+        pass
+
+    def _minimap_to_grid(self):
+        pass
+
+    def _draw_object(self):
+        pass
+
+    def _draw_cell(self)
+        pass
+    
+    def _process_connection(self):
+        pass
+
+    def _get_source_info(self):
+        pass
+
+    def _calculate_start(self):
+        pass
